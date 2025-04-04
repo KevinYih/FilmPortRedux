3e356f54a71ad84923eaa820befd57da165af6c2
keviny <kevinyih2022@gmail.com>
2025-04-04 00:24:17 -0700

    complete NavLink in header

--- src/components/FilmportLogo.jsx
+++ src/components/FilmportLogo.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const FilmportLogo = () => {
   return (
     <div className="flex items-center space-x-3">
-      {/* 图标方块 */}
+      {/* Icon Block */}
       <div className="relative w-11 h-11 border-4 border-orange-500 rounded-md shadow-lg">
         <div className="absolute top-1.5 left-1.5 w-1.5 h-1.5 bg-orange-500 rounded-full"></div>
         <div className="absolute top-1.5 right-1.5 w-1.5 h-1.5 bg-orange-500 rounded-full"></div>
       </div>
 
-      {/* 文字 Logo */}
+      {/* text Logo */}
       <h1 className="font-sans text-2xl font-bold tracking-wide text-slate-100">
         Film<span className="text-orange-500">Port</span>
       </h1>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import FilmportLogo from "./FilmportLogo";
+import { NavLink } from "react-router";
 
 const Header = () => {
+  const navigation = [
+    {
+      label: "TV Shows",
+      href: "tv",
+    },
+    {
+      label: "Movies",
+      href: "movie",
+    },
+  ];
+
   return (
-    <header className="fixed top-0 w-full h-16 bg-neutral-600 opacity-75 flex items-center">
-      <div className="container mx-auto px-10 ">
+    <header className="fixed top-0 w-full h-16 bg-neutral-600 opacity-75 flex  items-center">
+      <div className="container mx-auto flex items-center space-x-6 px-4">
         <FilmportLogo />
+        <nav className="hidden lg:flex space-x-6">
+          {navigation.map((nav, index) => {
+            return (
+              <div>
+                <NavLink key={nav.label} to={nav.href} className={({ isActive }) => `hover:text-neutral-100 ${isActive && "text-neutral-100"}`}>
+                  {nav.label}
+                </NavLink>
+              </div>
+            );
+          })}
+        </nav>
       </div>
     </header>
   );
